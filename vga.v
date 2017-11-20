ad9ff5f4102486b4dd4126ed8b604f7fa90b01fb
文艺小少年 <541190157@qq.com>
2017-11-20 22:17:13 +0800

    11.20

--- vga.v
+++ vga.v
@@ -1,18 +1,20 @@
-module vga(clk,hsync,vsync,rgb_r,rgb_g,rgb_b,CLK,VGA_SYNC,VGA_BLANK,TD_DATA,TD_VS,TD_HS,TD_CLK,TD_RESET_N);
+module vga(reset,clk,hsync,vsync,rgb_r,rgb_g,rgb_b,CLK,VGA_SYNC,VGA_BLANK,TD_DATA,TD_VS,TD_HS,TD_CLK,TD_RESET_N);
      input               TD_VS;
 	  input        [7:0]  TD_DATA;
 	  input               TD_HS;
 	  input               TD_CLK;
 	  input               clk;
+	  input               reset;
 	  output              TD_RESET_N;
 	  output   reg        hsync;
 	  output   reg        vsync;
-	  output   reg [7:0]  rgb_r;
-	  output   reg [7:0]  rgb_g;
-	  output   reg [7:0]  rgb_b;
-	  output              CLK;
+	  output   wire  [7:0] rgb_r ;
+	  output   wire  [7:0] rgb_g ;
+	  output   wire  [7:0] rgb_b ;
+	  output   reg        CLK;
 	  output              VGA_SYNC;
 	  output              VGA_BLANK;
+	  
 	  
 	  
 	  reg       [10:0]  x_cnt;
@@ -25,58 +27,77 @@ module vga(clk,hsync,vsync,rgb_r,rgb_g,rgb_b,CLK,VGA_SYNC,VGA_BLANK,TD_DATA,TD_V
 	  wire              c_dis;
 	  wire              d_dis;
      wire              e_rdy;
-	  reg               clk_n;
 	  assign    TD_RESET_N = 1'B1;
 	  assign    VGA_SYNC   = 1'b1;
-	  assign    vga_BLANK  = 1'B0;
-	  assign ypos= y_cnt-11'd35;
-	  assign xpos= x_cnt-11'd144;
-	  initial
+	  assign    vga_BLANK  = ~((x_cnt<H_BLANK)||(y_cnt<V_BLANK));
+	  assign    ypos= y_cnt-11'd35;
+	  assign    xpos= x_cnt-11'd144;
+	  parameter	H_FRONT	=	16;
+     parameter	H_SYNC	=	96;
+     parameter	H_BACK	=	48;
+     parameter	H_ACT	=	640;
+     parameter	H_BLANK	=	H_FRONT+H_SYNC+H_BACK;
+     parameter	H_TOTAL	=	H_FRONT+H_SYNC+H_BACK+H_ACT;
+	  
+     parameter	V_FRONT	=	10;
+     parameter	V_SYNC	=	2;
+     parameter	V_BACK	=	33;
+     parameter	V_ACT	=	480;
+     parameter	V_BLANK	=	V_FRONT+V_SYNC+V_BACK;
+     parameter	V_TOTAL	=	V_FRONT+V_SYNC+V_BACK+V_ACT; 
+	 /* initial
 	  begin
-	  rgb_r = 0;
-	  rgb_g = 0;
-	  rgb_b = 0;
-	  hsync = 1'b1;
-	  vsync = 1'b1;
+	  hsync = 1;
+	  vsync = 1;
 	  x_cnt = 0;
 	  y_cnt = 0;
+	  rgb_r = 8'd0;
+	  rgb_g = 8'd0;
+	  rgb_b = 8'd0;
+	  end */
+	  always @(posedge clk )
+	  begin
+	  CLK = ~CLK ;
 	  end
-	  assign CLK = clk ;
 
-     always @(posedge clk)
+     always @(posedge CLK)
 	  begin
-	  if(x_cnt < 11'd1687) x_cnt = x_cnt + 1'b1;
-	  else if(x_cnt == 11'd1687 )x_cnt = 11'd0;
-	  end 
-	  always @(posedge clk)
+	  if(!reset)
 	  begin
-	  if(y_cnt < 11'd1065) y_cnt = y_cnt + 1'd1;
-	  else if(y_cnt == 11'd1065)   y_cnt = 11'd0;
-	  end	  
-	  always @(posedge clk)
-	  begin
-	  if(x_cnt==47) hsync <= 1'b0;
-	  else if(x_cnt==11'd159) hsync <= 1'b1;
-     end
-	  always @(posedge clk)
-	  begin
-	  if(y_cnt==0) vsync <= 1'b0;
-	  else if (y_cnt==11'd3) vsync <=1'b1;
+	  x_cnt <= 0;
+	  hsync <= 1'b1;
 	  end
-	  always @ ( posedge clk ) 
-     begin                     
-        valid <=    ( ( x_cnt >= 10'd144 ) && ( x_cnt <= 10'd784) && ( y_cnt >= 10'd35)   && ( y_cnt <= 10'd515) );  
-     end
-	  assign a_dis=((xpos>=200)&&(xpos<=220))&&((ypos>=140)&&(ypos<=460));
-	  assign b_dis=((xpos>=580)&&(xpos<=600))&&((ypos>=140)&&(ypos<=460));
-	  assign c_dis=((xpos>=220)&&(xpos<=580))&&((ypos>=140)&&(ypos<=460));
-	  assign d_dis=((xpos>=220)&&(xpos<=580))&&((ypos>=440)&&(ypos<=460));
-	  assign e_dis=((xpos>=385)&&(xpos<=415))&&((ypos>=285)&&(ypos<=315));
-	  always@(posedge clk )
+	  else
+	  begin 
+	  if(x_cnt < H_TOTAL) x_cnt <= x_cnt + 1'b1;
+	  else x_cnt <= 11'd0;	  
+	  if(x_cnt == H_FRONT-1) hsync <= 1'b0;
+	  if (x_cnt == H_FRONT+H_SYNC-1)hsync <= 1'b1;
+	  end
+	  end
+	  always @(posedge hsync )
 	  begin
-	  if (valid & e_rdy) rgb_r <=8'b11110000;
-	  else if (valid & (a_dis | b_dis | c_dis | d_dis)) rgb_g <= 8'b11110000;
-	  else if (valid & ~(a_dis | b_dis | c_dis | d_dis)) rgb_b <= 8'b11110000;
+	  if(!reset)
+	  begin
+	  y_cnt <= 0;
+	  vsync <= 1'b1;
+	  end
+	  else
+	  begin 
+	  if(y_cnt < V_TOTAL) y_cnt <= y_cnt + 1'd1;
+	  else    y_cnt <= 11'd0;   
+	  if(y_cnt == V_FRONT-1) vsync <= 1'b0;
+	  if(y_cnt == V_FRONT+V_SYNC-1) vsync <=1'b1;
+	  end
+	  end
+	  always @ ( posedge CLK ) 
+     begin                     
+        valid <=    ( ( x_cnt >= 11'd50 ) && ( x_cnt <= 11'd640) && ( y_cnt >= 11'd35)   && ( y_cnt <= 11'd480) );  
      end
+
+	 assign rgb_r=8'd1;
+    assign rgb_g=valid?0:0;
+    assign rgb_b=valid?0:0;
+
 	  endmodule
 	  
